eb7989e9896b1349563526b3bb1799b941062d2a
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-05 08:47:50 -0400

    Add sincle project links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,8 +2,11 @@ import './Project.css'
 import Project from '../SingleProject/SingleProject'
 
 const Projects = () => {
-  const projects = [{title: 'Lavish Living Hotel', tech: ['JavaScript', 'HTML', 'CSS'], img:'https://placekitten.com/200/300'}]
-  const projectEls = projects.map((project, i) => (<Project key={i} src={project.img} title={project.title} tech={project.tech}/> ))
+  const projects = [
+    {title: 'Overlook', tech: ['JavaScript', 'HTML', 'CSS'], img:'https://placekitten.com/300/200', link: 'https://lauraguerra1.github.io/overlook/', gh: 'https://github.com/lauraguerra1/overlook'}
+  ]
+  
+  const projectEls = projects.map((project, i) => (<Project key={i} src={project.img} title={project.title} tech={project.tech} link={project.link} gh={project.gh}/> ))
   return (
     <section className='column-flex project-section'>
       <h1>PROJECTS</h1>
--- src/components/SingleProject/SingleProject.js
+++ src/components/SingleProject/SingleProject.js
@@ -1,6 +1,13 @@
+import Linkbar from '../Linkbar/Linkbar'
 import './SingleProject.css'
 
-const SingleProject = ({src, title, tech}) => {
+import github from '../../images/github-icon.png'
+import linkIcon from '../../images/link-icon.png'
+
+const SingleProject = ({src, title, tech, link, gh}) => {
+  const deployLink = {logo: linkIcon, location: `Laura Guerra\'s deployed ${title} project`, href: link}
+  const repoLink = {logo: github, location: `Laura Guerra\'s ${title} project repository`, href: gh}
+  
   const techStack = tech.map((item, i) => {
     return i === tech.length - 1
       ? item
@@ -8,12 +15,13 @@ const SingleProject = ({src, title, tech}) => {
   })
   return (
     <section className='single-project'>
+      <img className='project-img' src={src} alt={`Image of the ${title} project`}/>
       <div className='descriptive-section single-project-section'>
         <h2>{title.toUpperCase()}</h2>
+        <Linkbar links={[deployLink, repoLink]}/>
         <p>Techologies:</p>
         <p>{techStack.join(' ')}</p>
       </div>
-      <img src={src} alt={`Image of the ${title} project`}/>
     </section>
   )
 }
