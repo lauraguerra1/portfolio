efe2f8feac60c01454bcd7e107f00cdfcbc864d5
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-04 22:31:47 -0400

    Add media query for contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,14 +10,16 @@ const Contact = ({mainLinks}) => {
   return (
     <section className='column-flex descriptive-section'>
       <h1>CONTACT</h1>
-      <div>
+      <div className='contact-details-container'>
         <img className='brand-logo' src={lggLogo} alt='Laura Garcia Guerra brand logo'/>
-          <div className='meeting'>
-            <p>Let's Meet:</p>
-            <Link src={schedule} location={'Laura Guerra\'s Schedule'} href={'https://calendly.com/laura-guerra-calendar/30min'}/>
-          </div>
-          <p>Email: l.garciaguerra1@gmail.com</p>
-          <p>Phone: (310)770-6009</p>
+        <div className='contact-details'>
+            <div className='meeting'>
+              <p>Let's Meet:</p>
+              <Link src={schedule} location={'Laura Guerra\'s Schedule'} href={'https://calendly.com/laura-guerra-calendar/30min'}/>
+            </div>
+            <p>Email: l.garciaguerra1@gmail.com</p>
+            <p>Phone: (310)770-6009</p>
+        </div>
       </div>
       <Linkbar links={mainLinks}/>
     </section>
