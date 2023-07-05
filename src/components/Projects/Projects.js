e9dd5eae275e7d103735cdf3699ef16d0f4dfb7b
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-05 09:08:34 -0400

    Style scrollbar, main project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,15 +3,20 @@ import Project from '../SingleProject/SingleProject'
 
 const Projects = () => {
   const projects = [
+    {title: 'Overlook', tech: ['JavaScript', 'HTML', 'CSS'], img:'https://placekitten.com/300/200', link: 'https://lauraguerra1.github.io/overlook/', gh: 'https://github.com/lauraguerra1/overlook'},
+    {title: 'Overlook', tech: ['JavaScript', 'HTML', 'CSS'], img:'https://placekitten.com/300/200', link: 'https://lauraguerra1.github.io/overlook/', gh: 'https://github.com/lauraguerra1/overlook'},
+    {title: 'Overlook', tech: ['JavaScript', 'HTML', 'CSS'], img:'https://placekitten.com/300/200', link: 'https://lauraguerra1.github.io/overlook/', gh: 'https://github.com/lauraguerra1/overlook'},
     {title: 'Overlook', tech: ['JavaScript', 'HTML', 'CSS'], img:'https://placekitten.com/300/200', link: 'https://lauraguerra1.github.io/overlook/', gh: 'https://github.com/lauraguerra1/overlook'}
   ]
   
-  const projectEls = projects.map((project, i) => (<Project key={i} src={project.img} title={project.title} tech={project.tech} link={project.link} gh={project.gh}/> ))
+  const projectEls = projects.map((project, i) => (<Project key={i} src={project.img} title={project.title} tech={project.tech} link={project.link} gh={project.gh} index={i}/> ))
   return (
-    <section className='column-flex project-section'>
-      <h1>PROJECTS</h1>
+    <>
+    <h1>PROJECTS</h1>
+    <section className='project-section'>
       {projectEls}
     </section>
+    </>
   )
 }
 
--- src/components/SingleProject/SingleProject.js
+++ src/components/SingleProject/SingleProject.js
@@ -4,7 +4,7 @@ import './SingleProject.css'
 import github from '../../images/github-icon.png'
 import linkIcon from '../../images/link-icon.png'
 
-const SingleProject = ({src, title, tech, link, gh}) => {
+const SingleProject = ({src, title, tech, link, gh, index}) => {
   const deployLink = {logo: linkIcon, location: `Laura Guerra\'s deployed ${title} project`, href: link}
   const repoLink = {logo: github, location: `Laura Guerra\'s ${title} project repository`, href: gh}
   
@@ -13,15 +13,19 @@ const SingleProject = ({src, title, tech, link, gh}) => {
       ? item
       : `${item},`
   })
+
+  const projectImg = <img key={index + 'a'} className='project-img' src={src} alt={`Image of the ${title} project`}/>
+  const description = 
+  <div key={index + 'b'} className='descriptive-section single-project-section'>
+    <h2>{title.toUpperCase()}</h2>
+    <Linkbar links={[deployLink, repoLink]}/>
+    <p>Techologies:</p>
+    <p>{techStack.join(' ')}</p>
+  </div>
+  
   return (
     <section className='single-project'>
-      <img className='project-img' src={src} alt={`Image of the ${title} project`}/>
-      <div className='descriptive-section single-project-section'>
-        <h2>{title.toUpperCase()}</h2>
-        <Linkbar links={[deployLink, repoLink]}/>
-        <p>Techologies:</p>
-        <p>{techStack.join(' ')}</p>
-      </div>
+      {index % 2 === 0 ? [projectImg, description] : [description, projectImg]}
     </section>
   )
 }
