c29d6cef7f98d99c2ee0d9e9fe37d1c2f2897095
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-04 22:19:50 -0400

    Style contact page

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,27 @@
+import './Contact.css'
+
+import Linkbar from '../Linkbar/Linkbar'
+import Link from '../Link/Link'
+
+import lggLogo from '../../images/lgg-logo.png'
+import schedule from '../../images/schedule-icon.png'
+
+const Contact = ({mainLinks}) => {
+  return (
+    <section className='column-flex descriptive-section'>
+      <h1>CONTACT</h1>
+      <div>
+        <img className='brand-logo' src={lggLogo} alt='Laura Garcia Guerra brand logo'/>
+          <div className='meeting'>
+            <p>Let's Meet:</p>
+            <Link src={schedule} location={'Laura Guerra\'s Schedule'} href={'https://calendly.com/laura-guerra-calendar/30min'}/>
+          </div>
+          <p>Email: l.garciaguerra1@gmail.com</p>
+          <p>Phone: (310)770-6009</p>
+      </div>
+      <Linkbar links={mainLinks}/>
+    </section>
+  )
+}
+
+export default Contact 
