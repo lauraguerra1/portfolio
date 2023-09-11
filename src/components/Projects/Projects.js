965ddc9e16a6778589d8d62cd91d0330df5f29f8
Laura Guerra <l.garciaguerra1@gmail.com>
2023-09-11 18:04:07 -0400

    Add descriptions to projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,18 +13,18 @@ import projectPlanner from '../../images/projectplanner.png'
 
 const Projects = () => {
   const projects = [
-    {title: 'Project Planner', tech: ['React, TypeScript', 'Open AI'], img:projectPlanner, link: 'https://project-planner-ui.vercel.app/', gh:'https://github.com/lauraguerra1/project-planner-ui' },
-    {title: 'Style Stash', tech: ['Express.js', 'React', 'HTML', 'CSS', 'Router'], img:stylestash, link: 'https://style-stash.vercel.app/', gh: 'https://github.com/lauraguerra1/style-stash'},
-    {title: 'Pawfect Match', tech: ['React', 'TypeScript', 'HTML', 'CSS', 'Router', 'Cypress'], img: pawfect, link:'https://pawfect-match-laura.vercel.app/', gh:'https://github.com/lauraguerra1/pawfect-match'},
-    {title: 'Makeup 360', tech: ['React', 'TypeScript', 'HTML', 'CSS', 'Router', 'Cypress'], img:makeup360, link: 'https://makeup-360.vercel.app/', gh: 'https://github.com/lauraguerra1/makeup-360'},
-    {title: 'Rancid Tomatillos', tech: ['React', 'HTML', 'CSS', 'Router', 'Cypress'], img:rancid, link: 'https://rancid-tomatillos-laura.vercel.app/', gh: 'https://github.com/lauraguerra1/rancid-tomatillos'},
-    {title: 'Overlook', tech: ['JavaScript', 'HTML', 'CSS', 'Mocha/Chai'], img: overlook, link: 'https://lauraguerra1.github.io/overlook/', gh: 'https://github.com/lauraguerra1/overlook', instructions: 'username: customer50,password: overlook2021'},
-    {title: 'What\'s Cookin', tech: ['JavaScript', 'HTML', 'CSS', 'Mocha/Chai'], img: whatscookin, link: 'https://lauraguerra1.github.io/whats-cookin/', gh: 'https://github.com/lauraguerra1/whats-cookin'},
-    {title: 'Tic-Tac-Toe', tech: ['TypeScript', 'React'], img: tictactoe, link: 'https://laura-tic-tac-toe.vercel.app/', gh: 'https://github.com/lauraguerra1/tic-tac-toe'},
-    {title: 'Rock Paper Scissors', tech: ['JavaScript', 'HTML', 'CSS'], img: rockpaperscissors, link: 'https://lauraguerra1.github.io/rock-paper-scissors-project/', gh: 'https://github.com/lauraguerra1/rock-paper-scissors-project'}
+    {title: 'Project Planner', tech: ['React, TypeScript', 'Open AI'], img:projectPlanner, link: 'https://project-planner-ui.vercel.app/', gh:'https://github.com/lauraguerra1/project-planner-ui', description: 'An AI generator and organizational tool for developers to curate project plans.'},
+    {title: 'Style Stash', tech: ['Express.js', 'React', 'HTML', 'CSS', 'Router'], img:stylestash, link: 'https://style-stash.vercel.app/', gh: 'https://github.com/lauraguerra1/style-stash', description: 'A virtual closet for tracking clothing items and planning outfits.'},
+    {title: 'Pawfect Match', tech: ['React', 'TypeScript', 'HTML', 'CSS', 'Router', 'Cypress'], img: pawfect, link:'https://pawfect-match-laura.vercel.app/', gh:'https://github.com/lauraguerra1/pawfect-match', description: 'An online guide to discovering the ideal pet companion based on your unique personality traits.'},
+    {title: 'Makeup 360', tech: ['React', 'TypeScript', 'HTML', 'CSS', 'Router', 'Cypress'], img:makeup360, link: 'https://makeup-360.vercel.app/', gh: 'https://github.com/lauraguerra1/makeup-360', description: 'A one-stop destination designed for exploring and shopping for the perfect cosmetics collection.'},
+    {title: 'Rancid Tomatillos', tech: ['React', 'HTML', 'CSS', 'Router', 'Cypress'], img:rancid, link: 'https://rancid-tomatillos-laura.vercel.app/', gh: 'https://github.com/lauraguerra1/rancid-tomatillos', description: 'Explore, filter, and preview movies with ease.'},
+    {title: 'Overlook', tech: ['JavaScript', 'HTML', 'CSS', 'Mocha/Chai'], img: overlook, link: 'https://lauraguerra1.github.io/overlook/', gh: 'https://github.com/lauraguerra1/overlook', instructions: 'username: customer50,password: overlook2021', description: 'A hotel booking website.'},
+    {title: 'What\'s Cookin', tech: ['JavaScript', 'HTML', 'CSS', 'Mocha/Chai'], img: whatscookin, link: 'https://lauraguerra1.github.io/whats-cookin/', gh: 'https://github.com/lauraguerra1/whats-cookin', description: 'The ultimate culinary companion for discovering and searching recipes.'},
+    {title: 'Tic-Tac-Toe', tech: ['TypeScript', 'React'], img: tictactoe, link: 'https://laura-tic-tac-toe.vercel.app/', gh: 'https://github.com/lauraguerra1/tic-tac-toe', description: 'A fun and simple game of tic tac toe with cuddly characters.'},
+    {title: 'Rock Paper Scissors', tech: ['JavaScript', 'HTML', 'CSS'], img: rockpaperscissors, link: 'https://lauraguerra1.github.io/rock-paper-scissors-project/', gh: 'https://github.com/lauraguerra1/rock-paper-scissors-project', description: 'Play against the computer in a classic game of Rock, Paper, Scissors!'}
   ]
   
-  const projectEls = projects.map((project, i) => (<Project rightOrLeft={i % 2=== 0 ? 'right' : 'left'} key={i} src={project.img} title={project.title} tech={project.tech} link={project.link} gh={project.gh} index={i} instructions={project.instructions ? project.instructions : ''}/> ))
+  const projectEls = projects.map((project, i) => (<Project projectDescription={project.description} rightOrLeft={i % 2=== 0 ? 'right' : 'left'} key={i} src={project.img} title={project.title} tech={project.tech} link={project.link} gh={project.gh} index={i} instructions={project.instructions ? project.instructions : ''}/> ))
   return (
     <div className='main-project-page'>
     <h1>PROJECTS</h1>
