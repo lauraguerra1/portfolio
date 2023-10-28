dd96692959379c428b5b0af8c385f4b7bd009249
Laura Guerra <l.garciaguerra1@gmail.com>
2023-10-27 23:38:08 -0400

    Style form

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -36,7 +36,7 @@ const Admin = ({ updateError, logIn }) => {
   const mainInputs = ['Title', 'Description', 'Tech', 'Link', 'Gh', 'Image'].map(input => {
     const updateInputValue = updateProject(input.toLowerCase());
     return (
-      <div className='column-flex' key={input}>
+      <div className='column-flex-default form-piece' key={input}>
         <label htmlFor={input.toLowerCase()} className='align-self-start'>{input === 'Gh' ? 'GitHub': input}</label>  
         <input type='text' id={input.toLowerCase()} name={input.toLowerCase()} onChange={updateInputValue} value={project[input.toLowerCase()]} required/>
       </div>
@@ -68,16 +68,8 @@ const Admin = ({ updateError, logIn }) => {
         //     }}
       // />
 
-      <form onSubmit={submitProject}>
-        <div className='column-flex'>
-          {/* <label>Title  <input type='text' name='title' /></label>
-          <input type='text' name='title' />
-          <input type='textarea' name='description' />
-          <input type='text' name='tech' />
-          <input type='text' name='link' />
-          <input type='text' name='gh' />
-          <input type='text' name='image' />
-          <input type='text' name='image' /> */}
+      <form onSubmit={submitProject} className='w-80 column-flex-default'>
+        <div className='display-grid'>
           {mainInputs}
         </div>
         <p>Login Instructions Needed? </p> 
@@ -92,13 +84,19 @@ const Admin = ({ updateError, logIn }) => {
         </div>
         {
           project.loginInfo &&
-          <div>
-              <input type='text' name='username' value={project.username} onChange={(e) => updateProject(e.target.name)(e)} required/>
-              <input type='text' name='password' value={project.password} onChange={(e) => updateProject(e.target.name)(e)} required/>
-
+          <div className='display-grid'>
+              <div className='column-flex-default form-piece'>
+                <label htmlFor='username' className='align-self-start'>Username</label>  
+                <input type='text' id='username' name='username' value={project.username} onChange={(e) => updateProject(e.target.name)(e)} required/>
+              </div>
+              <div className='column-flex-default form-piece'>
+                <label htmlFor='password' className='align-self-start'>Password</label>  
+                <input type='text' id='password' name='password' value={project.password} onChange={(e) => updateProject(e.target.name)(e)} required/>
+              </div>
           </div>
         }
-        <button>Add Project</button>
+                <button>Add Project</button>
+
       </form>
   )
 }
