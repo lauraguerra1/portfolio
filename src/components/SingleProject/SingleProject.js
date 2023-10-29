a3b5f50a25802d6088ef47121de230abb27fe99b
Laura Guerra <l.garciaguerra1@gmail.com>
2023-10-28 23:41:33 -0400

    Update data shape to match upcoming merge with DB

--- src/components/SingleProject/SingleProject.js
+++ src/components/SingleProject/SingleProject.js
@@ -1,6 +1,6 @@
 import Linkbar from '../Linkbar/Linkbar'
 import './SingleProject.css'
-
+// import lggLogo from '../../images/lgg-logo.png'
 import github from '../../images/github-icon.png'
 import linkIcon from '../../images/link.png'
 import { useState, useEffect } from 'react'
@@ -10,22 +10,16 @@ const SingleProject = ({projectDescription, rightOrLeft, src, title, tech, link,
   const [pictureFirst, setPictureFirst] = useState(() => index % 2 === 0 || window.innerWidth < 900)
   const deployLink = {logo: linkIcon, location: `Laura Guerra\'s deployed ${title} project`, href: link}
   const repoLink = {logo: github, location: `Laura Guerra\'s ${title} project repository`, href: gh}
-  
-  const techStack = tech.map((item, i) => {
-    return i === tech.length - 1
-      ? item
-      : `${item},`
-  })
 
-  const projectImg = <Link key={index + 'a'} src={src} location={deployLink.location} href={link}/>
+  const projectImg = <Link key={index + 'a'} src={src ? src: 'https://i.imgur.com/f0GwD3h.png'} location={deployLink.location} href={link}/>
   const description = 
     <div key={index + 'b'} className='descriptive-section single-project-section'>
       <h2>{title.toUpperCase()}</h2>
       <p>{projectDescription}</p>
       <p>Techologies:</p>
-      <p>{techStack.join(' ')}</p>
-      {instructions && <p>{instructions.split(',')[0]}</p>}
-      {instructions && <p>{instructions.split(',')[1]}</p>}
+      <p>{tech}</p>
+      {instructions && <p>username: {instructions.split(',')[0]}</p>}
+      {instructions && <p>password: {instructions.split(',')[1]}</p>}
       <Linkbar links={[deployLink, repoLink]}/>
     </div>
   console.log('instructions', instructions)
