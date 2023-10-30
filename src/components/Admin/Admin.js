1f9972b7e766de50bde660282e264e6cc70fcb04
Laura Guerra <l.garciaguerra1@gmail.com>
2023-10-30 15:10:05 -0400

    Move form to separate component and create getAuthorizedUser

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import jwtDecode from 'jwt-decode';
 import './Admin.css'
 import SingleProject from '../SingleProject/SingleProject';
+import NewProjectForm from './NewProjectForm/NewProjectForm';
 //import getAuthorizedUser from '../apiCalls.js';
 //create login function that sets user in app 
 
@@ -19,11 +20,8 @@ const Admin = ({ updateError, logIn }) => {
     password: ''
   }
   const [project, setProject] = useState(freshProject)
+  const [user, setUser] = useState({ isAuthorized: false })
 
-  useEffect(() => {
-    console.log('loading admin page ')
-  }, [])
- 
   const updateProject = (inputName) => {
     return (event) => setProject(prev => ({ ...prev, [inputName]: event.target.value }))
   }
@@ -34,85 +32,60 @@ const Admin = ({ updateError, logIn }) => {
     console.log({ title, description, link, gh, image, tech, instructions: loginInfo ? `${username},${password}` : null })
     setProject(freshProject)
   }
-  const mainInputs = ['Title', 'Description', 'Tech', 'Link', 'Gh', 'Image'].map(input => {
-    const updateInputValue = updateProject(input.toLowerCase());
-    return (
-      <div className='column-flex-default form-piece' key={input}>
-        <label htmlFor={input.toLowerCase()} className='align-self-start'>{input === 'Gh' ? 'GitHub': input}</label>  
-        <input type='text' id={input.toLowerCase()} name={input.toLowerCase()} onChange={updateInputValue} value={project[input.toLowerCase()]} required/>
-      </div>
-    )
-  })
-    return (
-        // <GoogleLogin
-        //     shape='pill'
-        //     size='large'
-        // onSuccess={async credentialResponse => {
-        //       console.log('jwt', credentialResponse.credential)
-        //   console.log('user', jwtDecode(credentialResponse.credential).sub)
-        //       // try {
-        //       //   const user = await getAuthorizedUser({jwt: credentialResponse.credential})
-                
-        //       //   if (user.isAuthorized) {
-        //       //     // logIn(user)
-        //       //     console.log('user is authorized ')
-        //       //   } else {
-        //       //     throw new Error('Login Failed. Please try again with different credentials.')
-        //       //   }
-                  
-        //       //   } catch (error) {
-        //       //       updateError(new Error('Login Failed. Please try again with different credentials.'))
-        //       //   }
-        //     }}
-        //     onError={() => {
-        //         updateError(new Error('Login Failed. Please try again with different credentials.'))
-        //     }}
-      // />
-      <>
-        <form onSubmit={submitProject} className='w-80 column-flex-default'>
-          <div className='display-grid'>
-            {mainInputs}
-          </div>
-          <div className='instructions-options'>
-            <p>Login Instructions Needed? </p> 
-            <div className='toggler'>
-              <button type='button' className={`${project.loginInfo ? 'selected-option' : 'unselected-option'} pointer yes`} onClick={() => {
-                setProject(prev => ({...prev, loginInfo: !prev.loginInfo}))
-              }}>Yes</button>
 
-              <button  type='button' className={`${project.loginInfo ? 'unselected-option' : 'selected-option'} pointer no`}onClick={() => {
-                setProject(prev => ({...prev, loginInfo: !prev.loginInfo}))
-              }}>No</button>
-            </div>
-          </div>
-          {
-            project.loginInfo &&
-            <div className='display-grid'>
-                <div className='column-flex-default form-piece'>
-                  <label htmlFor='username' className='align-self-start'>Username</label>  
-                  <input type='text' id='username' name='username' value={project.username} onChange={(e) => updateProject(e.target.name)(e)} required/>
-                </div>
-                <div className='column-flex-default form-piece'>
-                  <label htmlFor='password' className='align-self-start'>Password</label>  
-                  <input type='text' id='password' name='password' value={project.password} onChange={(e) => updateProject(e.target.name)(e)} required/>
-                </div>
-            </div>
-          }
-          <button>Add Project</button>
-        </form>
-        <SingleProject
-          title={project.title}
-          tech={project.tech}
-          projectDescription={project.description}
-          src={project.image}
-          link={project.link}
-          gh={project.gh}
-          instructions={project.loginInfo ? `${project.username},${project.password}` : null}
-          index={0}
-          // rightOrLeft={'left'}
-        />
-      </>
-  )
+  const adjustLoginNeeded = () => setProject(prev => ({...prev, loginInfo: !prev.loginInfo}))
+
+  useEffect(() => {
+    //try and see if there is a user either in localstorage or in cookies 
+  }, [])
+
+  return (
+    <>
+    {
+      !user.isAuthorized ? 
+        <GoogleLogin
+          shape='pill'
+          size='large'
+          onSuccess={async credentialResponse => {
+            console.log('jwt', credentialResponse.credential)
+            console.log('user', jwtDecode(credentialResponse.credential).sub)
+            // try {
+              //   const user = await getAuthorizedUser({jwt: credentialResponse.credential})
+              
+            //   if (user.isAuthorized) {
+            //       setUser(user)
+                //     // logIn(user)
+                //     console.log('user is authorized ')
+                //   } else {
+                  //     throw new Error('Login Failed. Please try again with different credentials.')
+                  //   }
+                  
+                  //   } catch (error) {
+                    //       updateError(new Error('Login Failed. Please try again with different credentials.'))
+                    //   }
+                  }}
+                  onError={() => {
+                    updateError(new Error('Login Failed. Please try again with different credentials.'))
+                  }}
+              />
+            : 
+              
+            <>
+            <NewProjectForm updateProject={updateProject} project={project} submitProject={submitProject} adjustLoginNeeded={adjustLoginNeeded} />
+    <SingleProject
+      title={project.title}
+      tech={project.tech}
+      projectDescription={project.description}
+      src={project.image}
+      link={project.link}
+      gh={project.gh}
+      instructions={project.loginInfo ? `${project.username},${project.password}` : null}
+      index={0}
+    />
+    </>
+  }
+  </>
+    )
 }
 
 export default Admin
---
+++ src/components/Admin/NewProjectForm/NewProjectForm.js
@@ -0,0 +1,58 @@
+import SingleProject from '../../SingleProject/SingleProject';
+
+const NewProjectForm = (updateProject, submitProject, project, adjustLoginNeeded) => {
+
+  const mainInputs = ['Title', 'Description', 'Tech', 'Link', 'Gh', 'Image'].map(input => {
+    const updateInputValue = updateProject(input.toLowerCase());
+    return (
+      <div className='column-flex-default form-piece' key={input}>
+        <label htmlFor={input.toLowerCase()} className='align-self-start'>{input === 'Gh' ? 'GitHub': input}</label>  
+        <input type='text' id={input.toLowerCase()} name={input.toLowerCase()} onChange={updateInputValue} value={project[input.toLowerCase()]} required/>
+      </div>
+    )
+  })
+
+  return (
+    <>
+      <form onSubmit={submitProject} className='w-80 column-flex-default'>
+        <div className='display-grid'>
+          {mainInputs}
+        </div>
+        <div className='instructions-options'>
+          <p>Login Instructions Needed? </p> 
+          <div className='toggler'>
+            <button type='button' className={`${project.loginInfo ? 'selected-option' : 'unselected-option'} pointer yes`} onClick={adjustLoginNeeded}>Yes</button>
+            
+            <button  type='button' className={`${project.loginInfo ? 'unselected-option' : 'selected-option'} pointer no`}onClick={adjustLoginNeeded}>No</button>
+          </div>
+        </div>
+        {
+          project.loginInfo &&
+          <div className='display-grid'>
+          <div className='column-flex-default form-piece'>
+          <label htmlFor='username' className='align-self-start'>Username</label>  
+          <input type='text' id='username' name='username' value={project.username} onChange={(e) => updateProject(e.target.name)(e)} required/>
+          </div>
+          <div className='column-flex-default form-piece'>
+          <label htmlFor='password' className='align-self-start'>Password</label>  
+          <input type='text' id='password' name='password' value={project.password} onChange={(e) => updateProject(e.target.name)(e)} required/>
+          </div>
+          </div>
+        }
+        <button>Add Project</button>
+      </form>
+      <SingleProject
+        title={project.title}
+        tech={project.tech}
+        projectDescription={project.description}
+        src={project.image}
+        link={project.link}
+        gh={project.gh}
+        instructions={project.loginInfo ? `${project.username},${project.password}` : null}
+        index={0}
+      />
+    </>
+  )
+}
+
+export default NewProjectForm
