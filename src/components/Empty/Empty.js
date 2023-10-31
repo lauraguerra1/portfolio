234e3209241eb8766129b0e2716fed13c8fcc1fa
Laura Guerra <l.garciaguerra1@gmail.com>
2023-10-30 21:19:44 -0400

    Add spinning logo to empty page

--- src/components/Empty/Empty.js
+++ src/components/Empty/Empty.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom"
 import './Empty.css'
+import lggLogo from '../../images/lgg-logo.png'
 
 const Empty = () => {
   return (
-    <div className='column-flex'>
-      <h2 style={{ marginTop: '10vh' }}>Whoops! Nothing to see here!</h2>
-      <Link to='/' className='link'>Go To Home Page</Link>
+    <div className='column-flex empty-page'>
+      <h2 style={{ marginTop: '10vh', textAlign: 'center'}}>Whoops! Nothing to see here!</h2>
+      <img className='spin' src={lggLogo} alt='Spinning logo for Laura Garcia Guerra, Software Engineer'/>
+      <Link to='/' className='empty-link'>Go To Home Page</Link>
     </div>
   )
 }
