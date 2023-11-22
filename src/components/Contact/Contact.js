8680bb0a9581ad8da90d11eb9541d4fe304afbd5
Laura Guerra <l.garciaguerra1@gmail.com>
2023-11-22 13:57:59 -0500

    Add contact info to contact section and move contact button styling to globals

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,16 +9,21 @@ import schedule from '../../images/schedule-icon.png'
 const Contact = ({mainLinks}) => {
   return (
     <section className='column-flex descriptive-section contact-section'>
-      <h1 className='page-header'>CONTACT</h1>
       <div className='contact-details-container'>
-        <img className='brand-logo' src={lggLogo} alt='Laura Garcia Guerra brand logo'/>
+        <div className='contact-info'>
+          <h1 style={{ textAlign: 'left' }}>CONTACT INFORMATION</h1>
+          <p style={{ textAlign: 'left', fontSize: '18px'}}>Email: l.garciaguerra1@gmail.com</p>
+          <p style={{ textAlign: 'left', fontSize: '18px' }}>Phone: 310-770-6009</p>
+          <a href='www.linkedIn.com' target='_blank'>Visit LinkedIn</a>
+          <a href='www.linkedIn.com' target='_blank'>Visit GitHub</a>
+        </div>
         <div className='contact-details'>
-            <div className='meeting'>
-              <p>Schedule a meeting here:</p>
-              <Link src={schedule} location={'Laura Guerra\'s Schedule'} href={'https://calendly.com/laura-guerra-calendar/30min'}/>
-            </div>
-            <p>Email: l.garciaguerra1@gmail.com</p>
-            <p>Phone: (310)770-6009</p>
+          <div className='meeting'>
+            <p>Schedule a meeting here:</p>
+            <Link src={schedule} location={'Laura Guerra\'s Schedule'} href={'https://calendly.com/laura-guerra-calendar/30min'}/>
+          </div>
+          <p>Email: l.garciaguerra1@gmail.com</p>
+          <p>Phone: (310)770-6009</p>
         </div>
       </div>
       <Linkbar links={mainLinks}/>
