bc59092975ecf03fd9efb551c25b3035a2f496f7
Laura Guerra <l.garciaguerra1@gmail.com>
2023-11-21 22:11:56 -0500

    Style home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,13 @@
 import Linkbar from '../Linkbar/Linkbar'
 import './Home.css'
-import cartoonLogo from '../../images/cartoon-brand-image.png'
+import codeLogo from '../../images/CodeLogo.png'
 
 const Home = ({smallScreen, mainLinks}) => { 
   return (
     <section className='column-flex home-section'>
-      <img className='brand-logo' src={cartoonLogo} alt='Cartoon image of Laura Garcia Guerra, Software Engineer'/>
-      <Linkbar links={mainLinks} />
+      <img className='brand-logo' src={codeLogo} alt='Cartoon image of Laura Garcia Guerra, Software Engineer'/>
+      <h2 id='homeName'>LAURA GARCIA GUERRA</h2>
+      <h3 id='homeSE'>software engineer</h3>
       {!smallScreen && 
         <a target='_blank' href='https://docs.google.com/document/d/1mkalo0tUL6Rm7mHgiLRLoHhdU0yokyzmL0WXbkr_d1U/edit?usp=sharing'><button className="resume-button" id='homeResumeBtn'>view resume</button></a>
       }
