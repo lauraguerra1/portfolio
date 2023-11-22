140a47afd20f3237ffca0d171191ddb45e92dc8b
Laura Guerra <l.garciaguerra1@gmail.com>
2023-11-22 15:07:27 -0500

    Make contact info section responsive

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,16 +6,18 @@ const Contact = () => {
     <section className='column-flex descriptive-section contact-section'>
       <div className='contact-details-container'>
         <div className='contact-info'>
-          <h1>CONTACT<br/>INFORMATION</h1>
-          <p style={{fontSize: '18px'}}>Email: l.garciaguerra1@gmail.com</p>
-          <p style={{fontSize: '18px'}}>Phone: 310-770-6009</p>
-          <a className='contact-btn' href='https://www.linkedin.com/in/laura-garcia-guerra/' target='_blank'>Visit LinkedIn</a>
-          <a className='contact-btn' href='https://github.com/lauraguerra1' target='_blank'>Visit GitHub</a>
+          <div>
+            <h1>CONTACT<br/>INFORMATION</h1>
+            <p style={{fontSize: '18px'}}>Email: l.garciaguerra1@gmail.com</p>
+            <p style={{fontSize: '18px'}}>Phone: 310-770-6009</p>
+          </div>
+          <div>
+            <a className='contact-btn' href='https://www.linkedin.com/in/laura-garcia-guerra/' target='_blank'>Visit LinkedIn</a>
+            <a className='contact-btn' href='https://github.com/lauraguerra1' target='_blank'>Visit GitHub</a>
+          </div>
         </div>
         <div className='contact-details'>
-          <div className='meeting'>
-           <Form />
-          </div>
+          <Form />
         </div>
       </div>
     </section>
