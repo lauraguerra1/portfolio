45c5d67d9361d2fda5de8a7e18692c5f0a068ef8
Laura Guerra <l.garciaguerra1@gmail.com>
2024-02-16 18:34:02 -0500

    Update resume link

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,22 @@
-import Linkbar from '../Linkbar/Linkbar'
-import './Home.css'
-import codeLogo from '../../images/CodeLogo.png'
+import Linkbar from '../Linkbar/Linkbar';
+import './Home.css';
+import codeLogo from '../../images/CodeLogo.png';
 
-const Home = ({smallScreen}) => { 
+const Home = ({ smallScreen }) => {
   return (
     <section className='column-flex home-section'>
-      <img className='brand-logo' src={codeLogo} alt='Cartoon image of Laura Garcia Guerra, Software Engineer'/>
+      <img className='brand-logo' src={codeLogo} alt='Cartoon image of Laura Garcia Guerra, Software Engineer' />
       <h2 id='homeName'>LAURA GARCIA GUERRA</h2>
       <h3 id='homeSE'>software engineer</h3>
-      {!smallScreen && 
-        <a target='_blank' href='https://docs.google.com/document/d/1mkalo0tUL6Rm7mHgiLRLoHhdU0yokyzmL0WXbkr_d1U/edit?usp=sharing'><button className="resume-button" id='homeResumeBtn'>view resume</button></a>
-      }
+      {!smallScreen && (
+        <a target='_blank' href='https://docs.google.com/document/d/1T5gVa1syhk4TKqsFlKqhSA3tvsqHR_7dG8xj1ZXh8mo/edit?usp=sharing'>
+          <button className='resume-button' id='homeResumeBtn'>
+            view resume
+          </button>
+        </a>
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
